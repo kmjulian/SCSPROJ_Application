f9ae33a7b9edd2083f7fceecd8d8f1f0a56bd713
Rhea Ayungon <ayungon.rhea@gmail.com>
2017-10-08 12:38:04 +0800

    changes

---
+++ WeMove/app/src/main/java/com/wemove/scsproj/wemove/MainSearch.java
@@ -0,0 +1,10 @@
+package com.wemove.scsproj.wemove;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rheaayungon on 05/10/2017.
+ */
+
+public class MainSearch extends AppCompatActivity {
+}
