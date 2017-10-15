8d81ffe1a5a2a05c52e5bfdfcf90dd83462b3b04
Rhea Ayungon <ayungon.rhea@gmail.com>
2017-10-15 21:37:41 +0800

    AYOKO NA :(

--- WeMove/app/src/main/java/com/wemove/scsproj/wemove/MainActivity.java
+++ WeMove/app/src/main/java/com/wemove/scsproj/wemove/MainActivity.java
@@ -3,24 +3,34 @@ package com.wemove.scsproj.wemove;
 import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Handler;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     private Button buttonMain;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonMain = (Button) findViewById(R.id.buttonMain);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         buttonMain.setOnClickListener(new View.OnClickListener() {
@@ -42,5 +52,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
