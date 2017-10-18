7a4bd97411140ca2468fc6abdf025ace5c97f50c
Rhea Ayungon <ayungon.rhea@gmail.com>
2017-10-18 12:01:12 +0800

    wemovenav - draft?
    starting from the scratch

--- WeMoveNavigation/app/src/main/java/com/wemove/scsproj/wemovenavigation/LoginActivity.java
+++ WeMoveNavigation/app/src/main/java/com/wemove/scsproj/wemovenavigation/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.wemove.scsproj.wemove;
+package com.wemove.scsproj.wemovenavigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -6,46 +6,45 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by rheaayungon on 18/10/2017.
+ */
 
-    public static final String TAG = "MainActivity";
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
 
     private Button buttonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         buttonMain = (Button) findViewById(R.id.buttonMain);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
 
         buttonMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
-                startActivity(intent);
-        }
-                                      });
-        TextView textViewRegisterUser;
-
-        textViewRegisterUser = (TextView)findViewById(R.id.textViewRegisterUser);
-
-        textViewRegisterUser.setOnClicListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+
+        TextView textViewRegisterUser;
+
+        textViewRegisterUser = (TextView) findViewById(R.id.textViewRegisterUser);
+
+        textViewRegisterUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ WeMoveNavigation/app/src/main/java/com/wemove/scsproj/wemovenavigation/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.wemove.scsproj.wemovenavigation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rheaayungon on 18/10/2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
