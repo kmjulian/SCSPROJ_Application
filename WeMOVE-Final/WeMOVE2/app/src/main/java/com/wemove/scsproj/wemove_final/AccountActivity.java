396cb4a5b21bec71aeac0a98150f324e96adc9ec
Rhea Ayungon <ayungon.rhea@gmail.com>
2017-10-25 18:48:32 +0800

    YHEY IMAGE BUTTON

---
+++ WeMOVE-Final/WeMOVE2/app/src/main/java/com/wemove/scsproj/wemove_final/AccountActivity.java
@@ -0,0 +1,8 @@
+package com.wemove.scsproj.wemove_final;
+
+/**
+ * Created by rheaayungon on 25/10/2017.
+ */
+
+public class AccountActivity {
+}
--- WeMOVE-Final/WeMOVE2/app/src/main/java/com/wemove/scsproj/wemove_final/MainActivity.java
+++ WeMOVE-Final/WeMOVE2/app/src/main/java/com/wemove/scsproj/wemove_final/MainActivity.java
@@ -1,13 +1,39 @@
 package com.wemove.scsproj.wemove_final;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButtonAcount;
+    ImageButton imageButtonHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButtonAcount = (ImageButton) findViewById(R.id.imageButtonAccount);
+
+        imageButtonAcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadViewActivity = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intentLoadViewActivity);
+            }
+        });
+
+        imageButtonHome = (ImageButton) findViewById(R.id.imageButtonHome);
+
+        imageButtonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadViewActivity = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intentLoadViewActivity);
+            }
+        });
     }
 }
