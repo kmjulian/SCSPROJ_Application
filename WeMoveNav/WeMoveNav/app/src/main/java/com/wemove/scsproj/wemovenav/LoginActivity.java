1fc07967b2e96dbfcc3ea1396aaf261ab90e1422
Rhea Ayungon <ayungon.rhea@gmail.com>
2017-10-31 13:06:48 +0800

    hahah ulit na naman

---
+++ WeMoveNav/WeMoveNav/app/src/main/java/com/wemove/scsproj/wemovenav/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.wemove.scsproj.wemovenav;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by rheaayungon on 31/10/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginButton = (Button) findViewById(R.id.loginbutton);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView textViewRegisterUser;
+
+        textViewRegisterUser = (TextView) findViewById(R.id.textViewRegisterUser);
+
+        textViewRegisterUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
