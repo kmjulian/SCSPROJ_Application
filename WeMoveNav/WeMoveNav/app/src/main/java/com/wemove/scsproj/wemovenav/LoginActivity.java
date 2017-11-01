4d6363e679282efef08af4e6d138f50c5ef8583b
Rhea Ayungon <ayungon.rhea@gmail.com>
2017-11-01 17:16:30 +0800

    TARAY

--- WeMoveNav/WeMoveNav/app/src/main/java/com/wemove/scsproj/wemovenav/LoginActivity.java
+++ WeMoveNav/WeMoveNav/app/src/main/java/com/wemove/scsproj/wemovenav/LoginActivity.java
@@ -3,6 +3,7 @@ package com.wemove.scsproj.wemovenav;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,13 +15,13 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG = "LoginActivity";
 
-    private Button loginButton;
+    private CardView loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginButton = (Button) findViewById(R.id.loginbutton);
+        loginButton = (CardView) findViewById(R.id.btnLogin);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
 
         TextView textViewRegisterUser;
 
-        textViewRegisterUser = (TextView) findViewById(R.id.registerUser);
+        textViewRegisterUser = (TextView) findViewById(R.id.regUser);
 
         textViewRegisterUser.setOnClickListener(new View.OnClickListener() {
             @Override
