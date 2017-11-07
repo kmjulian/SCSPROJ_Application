bc11ac9a292ce4257095d34d7c3a1d3c21cb8c6b
Rhea Ayungon <ayungon.rhea@gmail.com>
2017-11-07 15:55:09 +0800

    continue

--- WeMoveNav/WeMoveNav/app/src/main/java/com/wemove/scsproj/wemovenav/HomeActivity.java
+++ WeMoveNav/WeMoveNav/app/src/main/java/com/wemove/scsproj/wemovenav/HomeActivity.java
@@ -1,8 +1,26 @@
 package com.wemove.scsproj.wemovenav;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+
 /**
  * Created by rheaayungon on 02/11/2017.
  */
 
-public class HomeActivity {
+public class HomeActivity extends AppCompatActivity {
+
+    private CardView accountButton;
+    private CardView searchButton;
+    private CardView eventButton;
+    private CardView mapsButton;
+    private CardView helpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        accountButton = (Car)
+    }
 }
